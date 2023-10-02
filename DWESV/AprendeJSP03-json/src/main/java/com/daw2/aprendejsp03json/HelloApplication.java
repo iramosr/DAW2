804ccbc600c36d9c5867a6b8ba2ac17c871f8bd5
iramosr <iramosr02@iescastelar.com>
2023-10-02 19:27:32 +0200

    jsp03

---
+++ DWESV/AprendeJSP03-json/src/main/java/com/daw2/aprendejsp03json/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.daw2.aprendejsp03json;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
