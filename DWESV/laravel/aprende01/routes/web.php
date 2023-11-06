1e95d2c68a073f405eccfa85f45436aacfad069c
iramosr <iramosr02@iescastelar.com>
2023-11-06 19:39:49 +0100

    Add laravel

---
+++ DWESV/laravel/aprende01/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\SaludosController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hola-mundo', function () {
+    return view('hola-mundo');
+});
+
+Route::get('/saludo', function () {
+    return view('saludo')->with('nombre', 'Pepito Pérez López');
+});
+
+Route::get('/hola-mundo2', [SaludosController::class,'hola2']);
+
+Route::get('/saludo2', [SaludosController::class,'saludo2']);
+
+Route::get('/saludo3/{nombre}', [SaludosController::class,'saludo3']);
