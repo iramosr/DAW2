3ee3e1f8fa619db593d6b116c29bd9960eb77869
iramosr <iramosr02@iescastelar.com>
2024-01-08 19:40:46 +0100

    Add spring 4

---
+++ DWESV/Spring/INF_CASTELAR/src/main/java/com/daw2/inf_castelar/InfCastelarApplication.java
@@ -0,0 +1,13 @@
+package com.daw2.inf_castelar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfCastelarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfCastelarApplication.class, args);
+    }
+
+}
---
+++ DWESV/Spring/INF_CASTELAR/src/test/java/com/daw2/inf_castelar/InfCastelarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw2.inf_castelar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfCastelarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
